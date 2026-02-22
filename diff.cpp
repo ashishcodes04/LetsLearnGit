0b492c1229456b92e31a232184fa8a481d639a09
Ashish Bisht <ashishbisht221@gmail.com>
2026-02-23 00:16:14 +0530

    added implementation for diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -5,6 +5,12 @@ int main(){
 
     int a = 4;
     int b = 3;
+    
+    cout<<"The difference is :-"<<(a-b)<<endl;
 
-    cout<<"The sum is :-"<<(a-b)<<endl;
+    int c = 3;
+    int d = 2;
+
+    cout<<"The difference is :-"<<(c-d)<<endl;
 }
+
